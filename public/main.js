f433ed625d2029a09ac0655b827273aad63b955a
Ilia Muravev <degtyarev327@gmail.com>
2021-03-08 20:43:56 +0300

    import http module

--- public/main.js
+++ public/main.js
@@ -1,9 +1,13 @@
 'use strict'
 
+import {HttpRequest} from './modules/http.js';
+
 const application = document.getElementById("app");
 
 //Страница ленты
-// application.innerHTML = newsFeed(postsData);
+application.innerHTML = newsFeed(postsData);
 
 //Страница юзера
-application.innerHTML = profile(profileData);
+// application.innerHTML = profile(postData);
+
+window.http = new HttpRequest('http://localhost:8000');
