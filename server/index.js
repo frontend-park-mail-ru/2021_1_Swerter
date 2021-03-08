f2ef0f29689ddaeaf60f72d20758049822ab56a4
Ilia Muravev <degtyarev327@gmail.com>
2021-03-08 20:27:57 +0300

    add extensions support for web server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 const http = require('http');
 const fs = require('fs');
 const debug = require('debug');
+const path = require('path')
 
 const log = debug('server')
 
@@ -22,11 +23,26 @@ const server = http.createServer((req, res) => {
             return;
         }
 
+        let contentType = 'text/plain';
+
+        switch(path.extname(file)) {
+            case '.js':
+                contentType = 'text/javascript';
+                break;
+            case '.html':
+                contentType = 'text/html';
+                break;
+            case '.css':
+                contentType = 'text/css';
+                break;
+        }
+
+        res.setHeader('Content-Type', contentType)
         res.write(data);
         res.end();
     })
 });
 
-const port = 3000;
+const port = 8080;
 
 server.listen(port);
