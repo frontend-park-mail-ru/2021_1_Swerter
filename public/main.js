f0b8221ae89d471f3c7025790ed06976445b0406
Ilia Muravev <degtyarev327@gmail.com>
2021-03-09 09:36:22 +0300

    add login page layout

--- public/main.js
+++ public/main.js
@@ -5,9 +5,12 @@ import {HttpRequest} from './modules/http.js';
 window.application = document.getElementById("app");
 
 //Страница ленты
-application.innerHTML = newsfeedTemplate(postsData);
+// application.innerHTML = newsfeedTemplate(postsData);
 
 //Страница юзера
 // application.innerHTML = profileTemplate(postData);
+
+//Страница логина и регистрации
+application.innerHTML = loginregisterTemplate();
 
 window.http = new HttpRequest('http://localhost:8000');
