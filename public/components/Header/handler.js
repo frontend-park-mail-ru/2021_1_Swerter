ecbb82f446356035c278e0af49f348e08e907700
Ilia Muravev <degtyarev327@gmail.com>
2021-03-09 15:27:42 +0300

    add login and registration

--- public/components/Header/handler.js
+++ public/components/Header/handler.js
@@ -1,15 +1,26 @@
 (function () {
     function goProfile() {
-        console.log('Profile')
         application.innerHTML = profileTemplate(profileData);
     }
 
     function goNews() {
-        console.log('news')
         application.innerHTML = newsfeedTemplate(postsData);
+    }
+
+    function logout() {
+        sendLogoutRequest();
+    }
+
+    async function sendLogoutRequest() {
+        res = await http.post({ url: '/logout' })
+
+        if (res.status === 200) {
+            router.goLogin();
+        }
     }
 
     window.router.register(goProfile)
     window.router.register(goNews)
 
+    window.logout = logout;
 })()
