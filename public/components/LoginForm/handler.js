34fbf681c3392152c47ebe1a70bf928c86b7df2e
Ilia Muravev <degtyarev327@gmail.com>
2021-03-16 12:23:15 +0300

    improve login form validation

--- public/components/LoginForm/handler.js
+++ public/components/LoginForm/handler.js
@@ -7,24 +7,36 @@
     login = document.getElementsByName('login')[0];
     password = document.getElementsByName('password')[0];
 
-    if (login.checkValidity() === true && password.checkValidity() === true) {
+
+    let errorMsg = null;
+    if (login.checkValidity() !== true) {
+      errorMsg = 'Wrong login format';
+    } else if (password.checkValidity() !== true) {
+      errorMsg = 'Wrong password format';
+    }
+
+    if (errorMsg === null) {
       const data = {
         login: login.value,
         password: password.value,
       };
-
       sendLoginRequest(data);
+    } else {
+      displayLoginFormValidationError(errorMsg);
     }
   }
 
-  function displayLoginFormValidationError() {
+  function displayLoginFormValidationError(error) {
     if (Array.from(document.getElementsByClassName('login-form__div-error')).length === 0) {
       const sib = document.getElementsByClassName('login-form__div-item')[0];
       const par = sib.parentNode;
       const errorBox = document.createElement('div');
       errorBox.className = 'login-form__div-error';
-      errorBox.innerHTML = '<h2>Неверный логин или пароль!</h2>';
+      errorBox.innerHTML = `<h2>${error}</h2>`;
       par.insertBefore(errorBox, sib);
+    } else {
+      const errorBox = document.getElementsByClassName('login-form__div-error').item(0);
+      errorBox.innerHTML = `<h2>${error}</h2>`;
     }
   }
 
@@ -33,7 +45,7 @@
     if (res.status === 200) {
       router.goProfile();
     } else if (res.status == 403) {
-      displayLoginFormValidationError();
+      displayLoginFormValidationError('Wrong login or password');
     }
   }
 
