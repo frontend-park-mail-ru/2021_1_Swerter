c4215902d1b6cbb0553e12cdc617c84c24a485ed
Ilia Muravev <degtyarev327@gmail.com>
2021-04-27 11:55:46 +0300

    edit observable mixin a bit

--- public/modules/observable.js
+++ public/modules/observable.js
@@ -1,26 +1,28 @@
 const Observable = {
-  on(event, callback) {
-    if (!this.listeners) {
-      this.listeners = {};
+    on(event, callback) {
+        if (!this.listeners) {
+            this.listeners = {};
+        }
+
+        if (!this.listeners[event]) {
+            this.listeners[event] = [callback];
+            return;
+        }
+        this.listeners[event].push(callback);
+    },
+
+    off(event, callback) {
+        this.listeners[event] = this.listeners[event]
+            .filter(listener => listener !== callback);
+    },
+
+    emit(event, data) {
+        this.listeners[event].forEach(listener => {
+            listener(data);
+        });
     }
-
-    if (!this.listeners[event]) {
-      this.listeners[event] = [callback];
-      return;
-    }
-    this.listeners[event].push(callback);
-  },
-
-  off(event, callback) {
-    this.listeners[event] = this.listeners[event]
-        .filter(listener => listener !== callback);
-  },
-
-  emit(event, data) {
-    this.listeners[event].forEach(listener => { listener(data); });
-  }
 };
 
 export default function makeObservable(classObj) {
-  Object.assign(classObj.prototype, Observable);
+    Object.assign(classObj.prototype, Observable);
 };
